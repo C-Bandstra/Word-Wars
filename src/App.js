f7f99e133a0af170e84fa55adf90b688fcbe62b9
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-04 12:53:24 -0600

    Testing word extraction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+var Owlbot = require('owlbot-js');
+var randomWords = require('random-words');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = Owlbot("2c7169bd30a747ad94dcd30355c31578110dc7aa")
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+
+    }
+  }
+
+  // componentDidMount = () => {
+  //   for(var i = 0; i < 4; i++) {
+  //     const word = randomWords(1)
+  //     console.log(word)
+  //     client.define(word).then(data => console.log(data))
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
