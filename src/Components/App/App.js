9260cda06ff4ea78bea8916cddf09b74a57ae6f4
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-04 13:43:29 -0600

    Import login and render login

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Login from '../Login/Login'
 var Owlbot = require('owlbot-js');
 var randomWords = require('random-words');
 
@@ -25,6 +26,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login />
       </div>
     );
   }
