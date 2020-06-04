59b54240edc8bdf7ab33cc8353ec728f007ca39e
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-04 13:08:08 -0600

    Edit file paths

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 var Owlbot = require('owlbot-js');
 var randomWords = require('random-words');
@@ -25,19 +25,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
