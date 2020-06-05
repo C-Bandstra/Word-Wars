e320374dbe66a5bd22d7b12eaf71c30cfc453188
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-04 18:50:36 -0600

    Add login condition and method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,10 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Login from '../Login/Login'
+import HomePage from '../HomePage/HomePage'
+import { Route, Redirect} from "react-router-dom";
+
 var Owlbot = require('owlbot-js');
 var randomWords = require('random-words');
 
@@ -11,8 +14,16 @@ class App extends Component {
   constructor() {
     super();
     this.state ={
-
+      username: 'CBandstra', 
+      loggedIn: true
     }
+  }
+
+  logIn = (result, username) => {
+    this.setState({
+      username: username,
+      loggedIn: result
+    })
   }
 
   // componentDidMount = () => {
@@ -26,7 +37,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+        {this.state.loggedIn ?
+          <Redirect to="/homepage" /> :
+          <Redirect to="/" />}
+        <Route
+          exact path='/'
+          render={() => {
+            return <Login logIn={this.logIn} />
+          }}
+        />
+        <Route
+          exact path='/homepage'
+          render={() => {
+            return <HomePage username={this.state.username}/>
+          }}
+        />
       </div>
     );
   }
