a48d674e9c533eb04b1800bc3051e2652b531e31
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-05 22:47:18 -0600

    Temporary link

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './HomePage.css';
 import NavBar from '../NavBar/NavBar';
+import { Link } from "react-router-dom";
 
 const HomePage = (props) => {
   return (
     <div className='homepage'>
       <NavBar username={props.username}/>
-      <p>HomePage</p>
+      <Link className='start-btn' to='/quiz'>
+        Start!
+      </Link>
     </div>
     
   )
