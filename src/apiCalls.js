a680b04f1ddfa50009e27bc055e1e9df91b58b8d
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-08 00:09:11 -0600

    Add fetchWord error handling method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,5 @@
+const randomWords = require('random-words');
+
 export const fetchWords = async (words) => {
   const wordsData = await words.map(async (word) => {
     const response = await fetch(`https://owlbot.info/api/v4/dictionary/${word}`, {
@@ -6,7 +8,21 @@ export const fetchWords = async (words) => {
       }
     })
     const data = await response.json()
+    if(data.definitions === undefined) {
+      return fetchNewWord()
+    }
     return data
   })
   return wordsData
+}
+
+export const fetchNewWord = async () => {
+  const word = randomWords()
+  const response = await fetch(`https://owlbot.info/api/v4/dictionary/${word}`, {
+      headers: {
+        Authorization: "Token 2c7169bd30a747ad94dcd30355c31578110dc7aa"
+      }
+    })
+    const data = await response.json()
+    return data
 }
