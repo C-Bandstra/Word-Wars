0833327e960256204294dd8fbb0ef9ad37ac5bd5
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-08 00:12:56 -0600

    Pass quizNum through props

--- src/Components/QuizContainer/QuizContainer.js
+++ src/Components/QuizContainer/QuizContainer.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './QuizContainer.css';
 import NavBar from '../NavBar/NavBar';
 import QuizCard from '../QuizCard/QuizCard';
-import { Link } from "react-router-dom";
 
 const QuizContainer = (props) => {
 
-  
   return (
     <div className='quiz-container'>
       <NavBar username={props.username}/>
-      <QuizCard />
+      <QuizCard quizNum={props.quizNum} />
     </div>
   )
 }
