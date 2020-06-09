a2fbb4173189b7a90e70ddf9c0e51c39862b21bb
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-09 11:58:53 -0600

    Add testing blueprint

---
+++ src/Components/ProfileContainer/ProfileContainer.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ProfileContainer from './ProfileContainer'
+import { render, fireEvent} from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('Profile  Container', () => {
+  it('should render without crashing', () => {
+    
+  })
+})
