54c9abe3cbe260b32a2b3bddf6439a4f3c33841c
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-09 11:58:23 -0600

    Add profile testing

---
+++ src/Components/Profile/Profile.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Profile from './Profile'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('Profile', () => {
+
+  const userStats = {
+    average: 20,
+    wordCount: 40
+  }
+
+  it('should render without crashing', () => {
+    const { getByText } = render(<MemoryRouter><Profile userStats={userStats} username='CBandstra' /></MemoryRouter>);
+
+    const username = getByText('CBandstra');
+
+    expect(username).toBeInTheDocument();
+  })
+
+  it('should render user stats', () => {
+    const { getByText } = render(<MemoryRouter><Profile userStats={userStats} username='CBandstra' /></MemoryRouter>);
+
+    const average = getByText('20%');
+
+    expect(average).toBeInTheDocument();
+  })
+})
